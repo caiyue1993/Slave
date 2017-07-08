cb9c79dc75332964a6895c3d946c863fbed1c6b4
caiyue1993 <278060043@qq.com>
2017-07-08 15:54:18 +0800

    init repo

--- Sources/App/Config+Setup.swift
+++ Sources/App/Config+Setup.swift
@@ -1,11 +1,12 @@
 import FluentProvider
+import LeafProvider
 
 extension Config {
     public func setup() throws {
         // allow fuzzy conversions for these types
         // (add your own types here)
         Node.fuzzy = [Row.self, JSON.self, Node.self]
-
+        
         try setupProviders()
         try setupPreparations()
     }
@@ -13,6 +14,7 @@ extension Config {
     /// Configure providers
     private func setupProviders() throws {
         try addProvider(FluentProvider.Provider.self)
+        try addProvider(LeafProvider.Provider.self)
     }
     
     /// Add all models that should have their
--- Sources/App/Routes.swift
+++ Sources/App/Routes.swift
@@ -1,24 +1,13 @@
 import Vapor
+import LeafProvider
 
 extension Droplet {
     func setupRoutes() throws {
-        get("hello") { req in
-            var json = JSON()
-            try json.set("hello", "world")
-            return json
-        }
-
-        get("plaintext") { req in
-            return "Hello, world!"
-        }
-
-        // response to requests to /info domain
-        // with a description of the request
+        
+        // for debug use
         get("info") { req in
             return req.description
         }
-
-        get("description") { req in return req.description }
         
         try resource("posts", PostController.self)
     }
