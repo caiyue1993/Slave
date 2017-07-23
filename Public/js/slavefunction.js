32223e33f22539fad2f1799a3a97e77b7693811b
caiyue1993 <278060043@qq.com>
2017-07-23 17:07:30 +0800

    separate js file and html file

---
+++ Public/js/slavefunction.js
@@ -0,0 +1,47 @@
+
+addLoadEvent(prepareAddFunction);
+addLoadEvent(prepareSendPost);
+
+function prepareAddFunction() {
+    var addButton = document.getElementById("addButton");
+    addButton.onclick = function() {
+        var div = document.getElementById("customDiv");
+        var newDiv = div.cloneNode(true)
+        document.getElementById("userDiv").appendChild(newDiv);
+    }
+}
+
+function prepareSendPost() {
+    var postButton = document.getElementById("postButton");
+    var toPostForm = postButton.form;
+    postButton.onclick = function() {
+        var variableNames = document.getElementsByClassName("inputVariableName");
+        var variableTypes = document.getElementsByClassName("selectVariableType");
+        var data = {};
+        for (var i=0; i < variableNames.length; i++) {
+            data[variableNames[i].value] = variableTypes[i].value;
+        }
+        
+        var xhr = new XMLHttpRequest();
+        xhr.open(toPostForm.method, toPostForm.action, true)
+        xhr.setRequestHeader("Content-Type", "application/json; charset=UTF-8");
+        xhr.send(JSON.stringify(data));
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState == 4 && xhr.status==200){
+                alert(xhr.responseText);
+            }
+        }
+    }
+}
+
+function addLoadEvent(func) {
+    var oldonload = window.onload;
+    if (typeof window.onload != 'function') {
+        window.onload = func;
+    } else {
+        window.onload = function() {
+            oldonload();
+            func();
+        }
+    }
+}
